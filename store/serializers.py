d3ee8c64173628a4090ed44d3e79b4831785f270
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-16 17:13:14 +0530

    added custom serializer method such that this won't impact to Data Model, this is specific to the API model(the Data model & API model should be different for having no impacts if one changes)

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,5 @@
 
+from decimal import Decimal
 from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
@@ -6,3 +7,9 @@ class ProductSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     description = serializers.CharField(allow_blank=True)
     price = serializers.DecimalField(max_digits=5, decimal_places=2, source='unit_price')
+    discount_price = serializers.SerializerMethodField(method_name="calculate_discounted_price")
+
+    def calculate_discounted_price(self, obj):
+        if obj.unit_price > 9:
+            return obj.unit_price * Decimal(0.9)  # 10% discount
+        return obj.unit_price
