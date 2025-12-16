492c2c29dba12882678f97e08224381716d25f14
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-16 18:05:53 +0530

    added model serializers to avoid validation/defining multiple times for the same fields

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,11 +10,17 @@ from .serializers import ProductSerializer, ProductModelSerializer
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    query_set = Product.objects.select_related('collection').all()
-    serializer = ProductModelSerializer(query_set, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = ProductModelSerializer(data = request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        query_set = Product.objects.select_related('collection').all()
+        serializer = ProductModelSerializer(query_set, many=True, context={'request': request})
+        return Response(serializer.data)
 
 @api_view()
 def product_detail(request, product_id):
