1d4bd5d775f5971447274a4553243f4e241731ba
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-17 11:57:02 +0530

    filtering based on query parameters

--- store/views.py
+++ store/views.py
@@ -12,10 +12,17 @@ from .serializers import CollectionModelSerializer, ProductModelSerializer, Revi
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     serializer_class = ProductModelSerializer    
     lookup_field = "id"
     lookup_url_kwarg = "product_id"
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get("collection_id")
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['product_id']).count() > 0:
