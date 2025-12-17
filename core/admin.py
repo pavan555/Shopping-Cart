8dacf103d71bfc2d51cd636dc5a23e90a667d985
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-17 17:51:06 +0530

    registered custom user class and in admin panel also

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
-
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 from store.admin import ProductModelAdmin
 from store.models import Product
 from tags.models import TaggedItem
 
 # Register your models here.
 
+# admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
 
 class TagItemInline(GenericTabularInline):
     model = TaggedItem
