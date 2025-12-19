8f7267a2baef8752c455413fa7f0d52309abfbc5
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-19 22:03:26 +0530

    added images retrieving api with static & media folder

--- storefront/settings.py
+++ storefront/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -137,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
--- storefront/urls.py
+++ storefront/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 admin.site.site_header = "Storefront Admin"
 admin.site.site_title = "Storefront Admin Portal"
@@ -30,3 +34,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     # path('api-auth/', include('rest_framework.urls'))
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
