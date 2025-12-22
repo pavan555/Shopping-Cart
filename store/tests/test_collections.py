0bcbe7356861bab2a65c8ce1c8628f1e3bace579
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-22 13:02:40 +0530

    added user authentication -> AAA method

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -6,12 +6,15 @@ import pytest
 
 @pytest.fixture
 def create_collection(api_client):
-    def do_create_collection(collection, user=None):
-        if user is not None:
-            api_client.force_authenticate(user=user)
+    def do_create_collection(collection):
         return api_client.post('/store/collections/', collection)
     return do_create_collection
 
+@pytest.fixture
+def authenticate(api_client):
+    def do_authenticate(is_staff=False):
+        api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate
 
 
 @pytest.mark.django_db
@@ -24,17 +27,20 @@ class TestCreateCollection:
         # Assert the results are what we expect
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
     
-    def test_if_user_not_admin_returns_403(self, create_collection):
-        response = create_collection({"title": "A"}, user = {}) # Simulating a non-admin user
+    def test_if_user_not_admin_returns_403(self, create_collection, authenticate):
+        authenticate()
+        response = create_collection({"title": "A"}) # Simulating a non-admin user
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_user_admin_with_invalid_data_returns_400(self, create_collection):
-        response = create_collection({"title": ""}, user = User(is_staff=True))
+    def test_if_user_admin_with_invalid_data_returns_400(self, create_collection, authenticate):
+        authenticate(is_staff=True)
+        response = create_collection({"title": ""})  # Invalid data: title is required
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
     
-    def test_if_user_admin_with_valid_data_returns_201(self, create_collection):
-        response = create_collection({"title": "Amazing Collection"}, user = User(is_staff=True))
+    def test_if_user_admin_with_valid_data_returns_201(self, create_collection, authenticate):
+        authenticate(is_staff=True)
+        response = create_collection({"title": "Amazing Collection"})
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
