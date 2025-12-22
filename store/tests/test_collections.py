d109f0294851c29d212d640ee1039998ee6f15c8
pavan-mitratech <saipavan.dathi@mitratech.com>
2025-12-22 12:46:54 +0530

    added multiple assertions

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 import pytest
 
 
@@ -14,6 +15,29 @@ class TestCreateCollection:
         # Assert the results are what we expect
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
     
+    def test_if_user_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})  # Simulating a non-admin user
+        response = client.post('/store/collections/', {"title": "A"})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_admin_with_invalid_data_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {"title": ""})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+    
+    def test_if_user_admin_with_valid_data_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {"title": "Amazing Collection"})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
+
+
     @pytest.mark.skip(reason="Skipping test for demonstration purposes")
     def test_skip_this_test(self):
-        pytest.skip("Skipping test for demonstration purposes")
+        print("Skipping test for demonstration purposes")
